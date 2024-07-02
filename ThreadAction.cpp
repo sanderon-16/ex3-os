e1d55484982195abbf3c64621d5f229dd22405b1
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-07-02 15:23:16 +0300

    updated some files

---
+++ ThreadAction.cpp
@@ -0,0 +1,12 @@
+# include "ThreadAction.h"
+
+void *threadAction(JobContext *context) {
+
+    // loop map
+
+    // sort intermediate
+
+    // shuffle if the id is 0
+
+    // loop reduce
+}
---
+++ ThreadAction.h
@@ -0,0 +1,12 @@
+//
+// Created by TLP-299 on 02/07/2024.
+//
+
+#ifndef EX3_THREADACTION_H
+#define EX3_THREADACTION_H
+
+#include "Barrier.h"
+
+void *threadAction(void *arg);
+
+#endif //EX3_THREADACTION_H
