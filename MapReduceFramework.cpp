921c36ed816fd5d2f046a2c4cf27b397aff24fa2
Ron Sendrovich <ron.sendrovich.44@gmail.com>
2024-07-02 14:58:00 +0300

    made a skeleton

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,28 @@
+# include "MapReduceFramework.h"
+
+JobHandle startMapReduceJob(const MapReduceClient &client,
+                            const InputVec &inputVec, OutputVec &outputVec,
+                            int multiThreadLevel) {
+
+}
+
+void waitForJob(JobHandle job) {
+
+}
+
+void getJobState(JobHandle job, JobState *state) {
+
+}
+
+void closeJobHandle(JobHandle job) {
+
+}
+
+void emit2(K2 *key, V2 *value, void *context) {
+
+}
+
+void emit3(K3 *key, V3 *value, void *context) {
+
+}
+
