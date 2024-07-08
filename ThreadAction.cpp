1499d671f67155d1205b1f799e5b9613edb937c9
gugiel <guy1.lidor@gmail.com>
2024-07-08 14:43:43 +0300

    shuffle

--- ThreadAction.cpp
+++ ThreadAction.cpp
@@ -9,6 +9,7 @@ void *thread_action(void *context) {
     uint64_t input_vector_size = job_context->atomic_counter->load();
     input_vector_size = GET_MIDDLE_NUMBER(input_vector_size);
 
+
     // loop map
     while (*(job_context->atomic_counter) < input_vector_size) // exit when all the input was mapped
     {
@@ -17,17 +18,59 @@ void *thread_action(void *context) {
                                  job_context->input_vec->at(old_value).second, context);
     }
 
+
     // sort intermediate
-    std::sort(thread_context->intermediate_vec->begin(), thread_context->intermediate_vec->end(), compare_pairs);
+    std::sort(job_context->personal_vecs[thread_context->threadID]->begin(),
+              job_context->personal_vecs[thread_context->threadID]->end(), compare_pairs);
+
 
     // shuffle if the id is 0
     if (thread_context->threadID == 0) {
         job_context->stage = SHUFFLE_STAGE;
-        // todo fill this
+        //TODO ac=0
+
+        while (GET_MIDDLE_NUMBER(job_context->atomic_counter->load()) >
+               GET_RIGHT_NUMBER(job_context->atomic_counter->load())) {
+            job_context->shuffle_vec->emplace_back();
+
+            K2 *lowest_k2 = nullptr;
+            for (int i = 0; i < job_context->num_threads; i++) {
+                if (!job_context->personal_vecs[i]->empty() && lowest_k2 == nullptr) {
+                    lowest_k2 = job_context->personal_vecs[i]->front().first;
+                } else if (!job_context->personal_vecs[i]->empty()) {
+                    if (lowest_k2 > job_context->personal_vecs[i]->front().first) {
+                        lowest_k2 = job_context->personal_vecs[i]->front().first;
+                    }
+                }
+            }
+
+            for (int i = 0; i < job_context->num_threads; i++) {
+                if (!job_context->personal_vecs[i]->empty()) {
+                    if (!(job_context->personal_vecs[i]->front().first > lowest_k2)) {
+                        while (!(job_context->personal_vecs[i]->front().first > lowest_k2)) {
+                            job_context->shuffle_vec->back().push_back(
+                                    std::move(job_context->personal_vecs[i]->front()));
+                            job_context->personal_vecs[i]->erase(job_context->personal_vecs[i]->begin());
+                            job_context->atomic_counter++;
+                        }
+                    }
+                }
+            }
+        }
+
+        job_context->barrier->barrier();
+    } else {
+        job_context->barrier->barrier();
     }
 
 
-    // loop reduce
+    // reduce loop
+    while (*(job_context->atomic_counter) < input_vector_size) // exit when all the input was reduced
+    {
+        uint32_t old_value = (*(job_context->atomic_counter))++; // advance the atomic timer
+        job_context->client->map(job_context->input_vec->at(old_value).first,
+                                 job_context->input_vec->at(old_value).second, context);
+    }
 }
 
 /**
